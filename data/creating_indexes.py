7c9ce26432edbb0249c9ede3308bf0af08d210c7
Ahmed Ashraf <ahmedashraaff113@gmail.com>
2025-11-30 10:14:56 +0200

    created faiss indexes

---
+++ data/creating_indexes.py
@@ -0,0 +1,38 @@
+import numpy as np
+import faiss
+import requests
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+with open("data/files/chunks.json", "r", encoding="utf-8") as f:
+    chunks = json.load(f)
+
+vectors = []
+chunks_content = [c["content"] for c in chunks]
+
+embedding_url = "https://inference.meganova.ai/v1/embeddings"
+
+embedding_headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {os.environ.get('MEGANOVA_API_KEY')}",
+}
+
+embedding_data = {"model": "Qwen/Qwen3-Embedding-8B", "input": chunks_content}
+
+response = requests.post(embedding_url, headers=embedding_headers, json=embedding_data)
+if response.status_code == 200:
+    embeddings = response.json()["data"]
+    for item in embeddings:
+        vectors.append(item["embedding"])
+    print("Embeddings generated successfully.")
+
+vectors_np = np.array(vectors).astype("float32")
+faiss.normalize_L2(vectors_np)  # Normalize for Cosine Similarity
+
+index = faiss.IndexFlatIP(4096)
+index.add(vectors_np)
+
+faiss.write_index(index, r"data\files\index.faiss")
